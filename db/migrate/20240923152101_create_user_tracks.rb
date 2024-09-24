38a74730f8243d610f1ec231c71b2d5792807664
Ktento <kento07210525mo@outlook.jp>
2024-09-24 14:26:19 +0900

    カラムの参照が正しくなるように修正

--- db/migrate/20240923152101_create_user_tracks.rb
+++ db/migrate/20240923152101_create_user_tracks.rb
@@ -1,8 +1,8 @@
 class CreateUserTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :user_tracks do |t|
-      t.references :User, null: false, foreign_key: true
-      t.references :Group, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_20_040500) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_23_152859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.string "A_user_id"
+    t.string "B_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_tracks", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "track_id", null: false
+    t.integer "listen_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_tracks_on_group_id"
+    t.index ["track_id"], name: "index_group_tracks_on_track_id"
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,4 +52,38 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_20_040500) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tracks", force: :cascade do |t|
+    t.string "track_name"
+    t.string "track_category"
+    t.string "track_artist"
+    t.string "spotify_url"
+    t.string "youtube_url"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_tracks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_user_tracks_on_group_id"
+    t.index ["user_id"], name: "index_user_tracks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_id"
+    t.string "user_name"
+    t.string "user_pass"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "group_tracks", "groups"
+  add_foreign_key "group_tracks", "tracks"
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
+  add_foreign_key "user_tracks", "groups"
+  add_foreign_key "user_tracks", "users"
 end

a5e3653cc6f7877d6c5d4bc68367269c851ee297
Ktento <kento07210525mo@outlook.jp>
2024-09-25 19:13:44 +0900

    userstrackstableの修正

--- db/migrate/20240923152101_create_user_tracks.rb
+++ db/migrate/20240923152101_create_user_tracks.rb
@@ -2,7 +2,7 @@ class CreateUserTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :user_tracks do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
+      t.references :track, null: false, foreign_key: true
 
       t.timestamps
     end

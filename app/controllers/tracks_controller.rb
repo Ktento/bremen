343ffc1d07725cfa5341615601ebe64e7946df77
J22018-PC\J22018 <san-j22018@sist.ac.jp>
2024-09-26 23:48:14 +0900

    トラックIDとyoutubeURLから曲を検索し、DBに登録する処理を作成中

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -43,16 +43,58 @@ class TracksController < ApplicationController
     end
   end
 
-  # POST /tracks
+  # POST /tracks  トラックIDとyoutubeURLから曲を検索し、DBに登録する処理を作成中
   def create
-    @track = Track.new(track_params)
+    track_id = params[:track_id]
 
-    if @track.save
-      render json: @track, status: :created, location: @track
-    else
-      render json: @track.errors, status: :unprocessable_entity
+    if track_id.blank?
+      render json: { error: 'トラックIDを指定してください' }, status: :bad_request
+      return
+    end
+
+    begin
+      # Spotify APIの認証
+      RSpotify.authenticate("c88268e353d2472c8ca1167a66091f88", "4e5aed842f334262b3cc2691f44198cc")
+      ENV['ACCEPT_LANGUAGE'] = "ja"
+
+      # Spotifyからトラック情報を取得
+      track = RSpotify::Track.find(track_id)
+
+      # 複数アーティストの名前とIDを取得し、カンマ区切りで保存
+      artist_names = track.artists.map(&:name).join(", ")
+      artist_ids = track.artists.map(&:id).join(", ")
+
+      # アーティストが持つジャンルを結合
+      artist_genres = track.artists.flat_map(&:genres).uniq.join(", ")
+
+      # トラックが存在しない場合の処理
+      if track.nil?
+        render json: { error: "指定されたトラックが見つかりませんでした。" }, status: :not_found
+        return
+      end
+
+      # データベースに曲を保存
+      @track = Track.new(
+        track_name: track.name,
+        track_artist: artist.name, # アーティスト名を保存
+        track_category: artist_genres, # アーティストのジャンルを保存
+        spotify_url: track.external_urls['spotify'],
+        youtube_url: params[:youtube_url] || nil, # YouTube URLがあれば使用、なければnull
+        image_url: track.album.images.first['url'],
+        sp_track_id: track.id, # トラックIDを保存
+        sp_artist_id: artist.id # アーティストIDを保存
+      )
+
+      if @track.save
+        render json: @track, status: :created, location: @track
+      else
+        render json: @track.errors, status: :unprocessable_entity
+      end
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
     end
   end
+
 
   # PATCH/PUT /tracks/1
   def update

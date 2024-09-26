5d7ed131a6db9ab4c221eeae2e2d7479466f577a
Ktento <kento07210525mo@outlook.jp>
2024-09-27 03:49:42 +0900

    許可するホストを変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   #ホストとして許可するURLを設定
-  config.hosts << "bremen-production.up.railway.app"
+  config.hosts << "agile-adventure-production.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
